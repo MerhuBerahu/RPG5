bd8f0e15030ffbb42fcaceb81308a0fcefc7c12a
Kalidor <wmclaughlan@gmail.com>
2024-12-08 01:45:40 +0000

    first commit

---
+++ beings.py
@@ -0,0 +1,85 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+This module's deals with the Base being Class that players, NPC's
+and enemies inherit from
+"""
+
+import random
+#from races import *
+#from jobs import *
+
+class Character:
+
+    def __init__(self, race, sex,name, level, mainjob, supportjob,gold,inventory):
+        self.race = race
+        self.sex = sex
+        self.name = name
+        self.level = level
+        self.mainjob = mainjob
+        self.supportjob = supportjob
+        self.gold = gold
+        self.inventory = inventory
+
+
+class Player(Character):
+
+    def __init__(self, race, sex, name,  level, mainjob, supportjob,gold,inventory,currentexp,exptnl):
+        super().__init__(race, sex, name, level, mainjob, supportjob,gold,inventory)
+        self.currentexp = currentexp
+        self.exptnl = exptnl
+
+
+class Enemy(Character):
+    def __init__(self, race, sex, name,level, mainjob, supportjob,gold, inventory= None):
+        super().__init__(race, sex, name, level, mainjob, supportjob,gold,inventory)
+        if inventory is None:
+            self.inventory = []
+        else:
+            self.inventory = inventory
+
+    def loot(self):           
+        choices = []
+        for item, weight in self.inventory:
+            choices.extend( [item]*weight )
+        return random.choice(choices)    
+
+class NPC(Character):
+
+    def __init__(self, race, sex, name,  level, mainjob, supportjob,gold,inventory,quests,merchant = None):
+        super().__init__(race, sex, name, level, mainjob, supportjob,gold,inventory)
+        self.quests = quests
+        self.merchant = merchant
+
+#x = Enemy(goblin,goblin['race_name'],"Cricket",5,white_mage,black_mage,52,"stick")
+#print(x.race_name)
+
+
+#CHARACTER CREATION
+print("Lets create your Character")
+
+
+x = Character("Human", "female", "Marge", 10, "Paladin", "dark Knight", 36,"potion")
+y = Player("dwarf", "male", "Homer", 20, "Warrior", "White Mage",400,"superpotion!",123,15)
+monster1 = Enemy("Orc", "male", 25,"Paladin", "Warrior", "rogue",52,inventory = [('Matted Fur',70),('High Quality Fur',10)])
+
+
+print(x.race)
+
+print(y.inventory)
+print(monster1)
+print(monster1.inventory)
+print(monster1.loot()) 
+
+#def starting_stats():
+    #check race
+    #check level
+    #check mainjob
+    #check support job
+
+
+#def determine_stats():
+    #check race
+    #check level
+    #check mainjob
+    #check support job
---
+++ jobs.py
@@ -0,0 +1,27 @@
+class Job:
+
+    def __init__(self, name, description, hp, mp,spell_list,ability_list):
+        self.name = name
+        self.description = description
+        self.hp = hp
+        self.mp = mp
+        self.spell_list = spell_list
+        self.ability_list = ability_list
+
+
+whm = Job("White Mage", "Healer", 7, 8,["cure", "Dia"],["Dual Wield","Run"])
+
+print(whm.description)
+
+
+"""
+#White Mage
+white_mage = {'name': 'White Mage', 'description': 'Healer','hp':7 ,'mp':8 ,'spelllist': {
+'cure': {'name': 'Cure','description': 'Restore\'s HP','level': 1, 'effect': 'healing','amount':2, 'mp_cost': 8, 'cast_time': 3,'modifier':'playerMind','modifier_amount':10,'element': 'lightsday'},
+'dia': {'name': 'Dia','description' : 'Lowers an enemy\'s defense and gradually deals Light elemental damage.','level': 3, 'effect': 'enfeebling','amount':2, 'mp_cost': 7, 'cast_time': 3,'modifier':'enemyMind','modifier_amount':10,'element': 'lightsday'},
+'paralyze': {'name': 'Paralyze','description': 'Paralyzes an enemy.','level': 4, 'effect': 'enfeebling','amount':2, 'mp_cost': 6, 'cast_time': 3,'modifier':'enemyMind','modifier_amount':10,'element': 'lightsday'},
+'banish': {'name': 'Banish','description': 'Deals Light elemental damage to an enemy.','level': 5, 'effect': 'damage','amount':2, 'mp_cost': 15, 'cast_time': 3,'modifier':'playerMind','modifier_amount':10,'element': 'lightsday'},
+'barstonra': {'name': 'Barstonra','description': 'Increases resistance against Earth.','level': 5, 'effect': 'enhancing','amount':2, 'mp_cost': 12, 'cast_time': 1,'modifier':'playerMind','modifier_amount':10,'element': 'windsday'}},
+'abilities': {'rage':{'level': 1, 'effect': 'Damage x2, Defense halved', 'tp_cost': 100, 'cast_time':3,'modifier':'playerStrength'}}, 
+'armourtype': 'robes'}
+"""
