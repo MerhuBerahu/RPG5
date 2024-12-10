57687cc152e06b8057da096749b7a0af137b214a
Kalidor <wmclaughlan@gmail.com>
2024-12-10 00:34:04 +0000

    db and spells

--- spells.py
+++ spells.py
@@ -1,3 +1,5 @@
+import sqlite3
+
 class Spell:
 
     def __init__(self, name, description, jobs, level, effect, effect_amount,mp_cost, cooldown, cast_time, modifier, modifier_amount, element_affinity):
@@ -15,6 +17,31 @@ class Spell:
         self.element_affinity = element_affinity
 
 cure = Spell("Cure")
+
+
+
+# Create a SQL connection to our SQLite database
+conn = sqlite3.connect('Database.db')
+
+c = conn.cursor()
+
+def item_by_owned(owned):
+    # Return all results of query
+    c.execute('SELECT * FROM plots spells WHERE whm >= 1')
+    return c.fetchall()
+
+def print_inventory_names(inventory):
+    for i in inventory: #print out the name(index[1] of each item in inventory
+        print(i[1])   
+
+inventory = item_by_owned()
+
+# Return first result of query
+#cur.execute('SELECT species FROM species WHERE taxa="Bird"')
+#cur.fetchone()
+
+# Be sure to close the connection
+conn.close()
 
 
 
